19ef5b8a2adf2b1b066222410c9f432c008765b4
Rafael Juzo Gomes Oda <info@clitz.com.br>
2013-09-17 15:20:57 -0300

    listBuscar

---
+++ Muscled2/BuscarListViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuscarListViewController.h
+//  Muscled2
+//
+//  Created by Rafael Oda on 9/17/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuscarListViewController : UITableViewController
+
+@end
